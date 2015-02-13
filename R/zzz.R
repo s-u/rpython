9f2880605c3ad9461e1a04d93ea891e8e9f3320b
Simon Urbanek <simon.urbanek@R-project.org>
2015-02-13 16:32:34 -0500

    register missing rpy_rcaps symbol

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,7 @@
 ## wise none of its modules work since thay fail to link to Python)
 .onLoad <- function(libname, pkgname) {
   .sym <- c('rpy_init', 'rpy_get', 'rpy_set', 'rpy_eval', 'rpy_import', 'rpy_as_string',
-            'rpy_call', 'rpy_fetch_ex', 'rpy_last_ex', 'rpy_get_attr', 'rpy_set_attr')
+            'rpy_call', 'rpy_fetch_ex', 'rpy_last_ex', 'rpy_get_attr', 'rpy_set_attr', 'rpy_rcaps')
   library.dynam(pkgname, pkgname, libname, local=FALSE, now=FALSE)
   ## now we have to register symbols by hand *sigh*
   e <- environment(.onLoad)
